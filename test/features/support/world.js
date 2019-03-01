7e15b8778babf2816cd579471d40dddaa6adf1d1
student <110219student11>
2019-03-01 10:56:57 +0000

    new commit

---
+++ test/features/support/world.js
@@ -0,0 +1,9 @@
+'use strict';
+var World = function World() {
+  this.product = undefined;
+  this.openWebsite = function() {
+  browser.waitForAngularEnabled(false);
+  return browser.get('');
+  }
+};
+module.exports.World = World;
